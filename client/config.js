82936b070d99889184e3a173fa38db414b86a4f8
Zaydme <46847212+Zaydme@users.noreply.github.com>
2022-03-14 13:41:16 +0100

    making this less dependent on others

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   faucetAddress: '0x3c53c9A2d0b69bCFbA423B97Feb9d07fE097d344',
   hCaptchaSiteKey: '9bdebf08-4297-48dd-9306-38b6fd0218e5',
-  apiUrl: 'https://maticapi.btn.sh'
+  apiUrl: 'https://maticapi.zayd.info'
 }
